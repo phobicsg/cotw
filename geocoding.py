c7801b94d2c0f57c4f92d4d53a032f16e92d249c
phobicsg <77351383+phobicsg@users.noreply.github.com>
2021-10-24 18:58:30 -0500

    Update geocoding.py

--- geocoding.py
+++ geocoding.py
@@ -7,7 +7,7 @@ import random
 from datetime import datetime
 import csv
 import time
-import credentials
+from translatorApp import *
 
 
 
@@ -22,41 +22,50 @@ def getReverseGeocoding(area,country,countryCode, lang):
     area = area +","+country
     url = "https://maps.googleapis.com/maps/api/geocode/json?address=%s&language=%s&key=%s" % (area,lang, API_KEY)
     print(url)
-    response = requests.get(url)
-    result=response.json()
-    print(result)
-    if result["status"] == "ZERO_RESULTS":
-        print("No Results Found for %s. Consider Search by GPS Coordinates" % area)
-        return "",""
-    else:
-        placeDetails = result["results"]       
-        geoInfoObj = placeDetails[0]
-        addressComponentList=geoInfoObj["address_components"]
-        placeTypesList= geoInfoObj["types"]
-        if "political" in placeTypesList or "locality" in placeTypesList or "sublocality" in placeTypesList:
-            countryType=["country","political"]
-            country=findObjInListByAttributes(addressComponentList,"types",countryType)
-            if country is not None:
-                countryShortName = country["short_name"]
-                if countryCode == countryShortName:
-                    for addresses in addressComponentList:
-                        addressType=addresses["types"]
-                        if addressType==placeTypesList:
-                            translatedName=addresses["long_name"]
-                            print("Translated Name for %s is : %s" % (area, translatedName))
-                            latLng=geoInfoObj["geometry"]["location"]
-                            latLngList = [latLng["lat"], latLng["lng"]]
-                            print(latLngList)
-                            return translatedName ,latLngList
+    try:
+
+        response = requests.get(url)
+        result=response.json()
+        print(result)
+        if result["status"] == "ZERO_RESULTS":
+            print("No Results Found for %s. Consider Search by GPS Coordinates" % area)
+            return "",""
+        else:
+            placeDetails = result["results"]       
+            geoInfoObj = placeDetails[0]
+            addressComponentList=geoInfoObj["address_components"]
+            placeTypesList= geoInfoObj["types"]
+            if "political" in placeTypesList or "locality" in placeTypesList or "sublocality" in placeTypesList:
+                countryType=["country","political"]
+                country=findObjInListByAttributes(addressComponentList,"types",countryType)
+                if country is not None:
+                    countryShortName = country["short_name"]
+                    if countryCode == countryShortName:
+                        for addresses in addressComponentList:
+                            addressType=addresses["types"]
+                            if addressType==placeTypesList:
+                                translatedName=addresses["long_name"]
+                                print("Translated Name for %s is : %s" % (area, translatedName))
+                                latLng=geoInfoObj["geometry"]["location"]
+                                latLngList = [latLng["lat"], latLng["lng"]]
+                                print(latLngList)
+                                return translatedName ,latLngList
+                    else:
+                        print("Result return is not in the right country. Verify location by GPS")
+                        return "",""
                 else:
-                    print("Result return is not in the right country. Verify location by GPS")
+                    print("Country not found. Likely disputed territory")
                     return "",""
             else:
-                print("Country not found. Likely disputed territory")
-                return "",""
-        else:
-            print("no suitable match is found. Verify Location by GPS")
-            return "",""
+                print("no suitable match is found. Verify Location by GPS")
+                return "",""        
+
+    except:
+        e = sys.exc_info()
+        print(e)
+        return "",""
+    
+
 
 def populatingTranslation(listOfEntries,country,baseLangCode,translatedLangCode, countryCode):
     translatedList=[]
@@ -64,7 +73,7 @@ def populatingTranslation(listOfEntries,country,baseLangCode,translatedLangCode,
         english_name = place[baseLangCode]
         translatedName, latlng =getReverseGeocoding(english_name,country,countryCode,translatedLangCode)
         place[translatedLangCode]=translatedName
-        place["laglng"]=latlng
+        place["latlng"]=latlng
         translatedList.append(place)
     return translatedList
 
@@ -75,9 +84,30 @@ def extractingListByRange(listOfEntries, start,end):
         extractedList.append(listOfEntries[i])
     return extractedList
 
+def gettingTranslationByCountryAndLanguage(countrycode3, country, languageCode, outputFileName):
+    if isSupportedLanguageOnGoogle(languageCode):
+        listOfPlaces=getKeysForTargetLanguage(countrycode3,languageCode)
+        if len(listOfPlaces) < 2000: 
+            translatedList=populatingTranslation(listOfPlaces,country,"en",languageCode,getCountryCodeISO3166(country))
+            writeCSVFile(outputFileName,translatedList)
+        else:
+            print("There are a total of %d entries found. Consider slicing up the records and run your query for optimum time" % len(listOfPlaces))
+    else:
+        print("Requested Translated Language %s is not supported on Google Platform" % languageCode)
 
 
+def getListOfGoogleCompatibleTranslations():
+    listOfPlaces=readJSONFile(PLACES_TO_TRANSLATE)
+    newlist=[]
+    for place in listOfPlaces:
+        supportedlang=[]
+        languages = place["languages"]
+        for lang in languages:
+           if isGoogleMapSupportedLang(lang):
+               supportedlang.append(lang)
+        if len(supportedlang)!=0:
+            place["languages"] = supportedlang
+            newlist.append(place)
+    return newlist
 
-extractedDataForTranslation=extractingListByRange(readCSVFile("Input/NPL-ne-translations.csv"),0,5000)
-translatedList= populatingTranslation(extractedDataForTranslation,"Nepal","en","ne","NP")
-writeCSVFile("Output.csv",translatedList)
+
