1d7d824d9d847d44aa992bffaf7c39ae4f34540f
phobicsg <77351383+phobicsg@users.noreply.github.com>
2021-10-23 22:25:16 -0500

    Update geocoding.py

--- geocoding.py
+++ geocoding.py
@@ -16,40 +16,65 @@ def getGeocoding(lat,long,lang):
     print(url)
     response = requests.get(url)
     print(response)
-    print(response.content)
+    print(response.json())
     
-def getReverseGeocoding(area,countryCode, lang):
+def getReverseGeocoding(area,country,countryCode, lang):
+    area = area +","+country
     url = "https://maps.googleapis.com/maps/api/geocode/json?address=%s&language=%s&key=%s" % (area,lang, API_KEY)
     print(url)
     response = requests.get(url)
     result=response.json()
     print(result)
     if result["status"] == "ZERO_RESULTS":
-        return "No Results Found for %s. Consider Search by GPS Coordinates" % area
+        print("No Results Found for %s. Consider Search by GPS Coordinates" % area)
+        return "",""
     else:
-        placeDetails = result["results"]
-        if len(placeDetails)==1:
-            geoInfoObj = placeDetails[0]
-            addressComponentList=geoInfoObj["address_components"]
-            placeTypesList= geoInfoObj["types"]
-            if "political" or "locality" or "sublocality" in placeTypesList:
-                countryType=["country","political"]
-                country=findObjInListByAttributes(addressComponentList,"types",countryType)
-                if country is not None:
-                    countryShortName = country["short_name"]
-                    if countryCode == countryShortName:
-                        for addresses in addressComponentList:
-                            addressType=addresses["types"]
-                            if addressType==placeTypesList:
-                                translatedName=addresses["long_name"]
-                                print("Translated Name for %s is : %s" % (area, translatedName))
-                                return translatedName
-                    else:
-                        return "Result return is not in the right country. Verify location by GPS"
-            else:
-                return "no suitable match is found. Verify Location by GPS"
-   
-#listofentries = readCSVFile("Input/NPL-ne-translations.csv")
-#print(len(listofentries))
+        placeDetails = result["results"]       
+        geoInfoObj = placeDetails[0]
+        addressComponentList=geoInfoObj["address_components"]
+        placeTypesList= geoInfoObj["types"]
+        if "political" in placeTypesList or "locality" in placeTypesList or "sublocality" in placeTypesList:
+            countryType=["country","political"]
+            country=findObjInListByAttributes(addressComponentList,"types",countryType)
+            if country is not None:
+                countryShortName = country["short_name"]
+                if countryCode == countryShortName:
+                    for addresses in addressComponentList:
+                        addressType=addresses["types"]
+                        if addressType==placeTypesList:
+                            translatedName=addresses["long_name"]
+                            print("Translated Name for %s is : %s" % (area, translatedName))
+                            latLng=geoInfoObj["geometry"]["location"]
+                            latLngList = [latLng["lat"], latLng["lng"]]
+                            print(latLngList)
+                            return translatedName ,latLngList
+                else:
+                    print("Result return is not in the right country. Verify location by GPS")
+                    return "",""
+        else:
+            print("no suitable match is found. Verify Location by GPS")
+            return "",""
 
-print(getReverseGeocoding("Gairigaun","NP", "ne"))
+def populatingTranslation(listOfEntries,baseLangCode,translatedLangCode, countryCode):
+    translatedList=[]
+    for place in listOfEntries:
+        english_name = place[baseLangCode]
+        translatedName, latlng =getReverseGeocoding(english_name,countryCode,translatedLangCode)
+        place[translatedLangCode]=translatedName
+        place["laglng"]=latlng
+        translatedList.append(place)
+    return translatedList
+
+
+def extractingListByRange(listOfEntries, start,end):
+    extractedList=[]
+    for i in range(start,end):
+        extractedList.append(listOfEntries[i])
+    return extractedList
+
+
+
+
+extractedDataForTranslation=extractingListByRange(readCSVFile("Input/NPL-ne-translations.csv"),0,5000)
+translatedList= populatingTranslation(extractedDataForTranslation,"Nepal","en","ne","NP")
+writeCSVFile("Output.csv",translatedList)
